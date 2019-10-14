e7d4f8d30fdec545c5a1e0c1f9550fd85750d4c2
Candy1117 <pxb15653577393@163.com>
2019-10-14 11:12:04 +0800

    01.variable.sh

---
+++ 01.variable.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+#filepath:/pxb/code/shell/01.variable.sh
+#email:pxb15653577393@163.com
+#author:Candy
+#date:2019/10/14/10:11
+#modify_time:time@pass
+#usage:practice variable
+
+#variable01='hello world'
+#echo ${variable01}
+
+#variable02='123'
+#echo ${variable02}
+
+#variable03='3.14'
+#echo ${variable03}
+
+#variable04=hello 
+#echo ${variable04}
+
+#variable05=hello pwd
+#echo ${variable05}
